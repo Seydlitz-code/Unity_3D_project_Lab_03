c01869114b30678c31258a057928ce5ae0f332c2
Seydlitz-code <153933205+Seydlitz-code@users.noreply.github.com>
2024-06-23 22:25:53 +0900

    Lab 3 - 4
    게임의 특정 이벤트에서 적용되는 파티클 효과 추가
    게임 내 배경 오디오, 점프, 충돌 시 오디오 이벤트 추가

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -10,12 +10,18 @@ public class PlayerController : MonoBehaviour
 	public bool isOnGround = true;
 	public bool gameOver = false;
 	private Animator playerAnim;
+	public ParticleSystem explosionParticle;
+	public ParticleSystem dirtParticle;
+	public AudioClip jumpSound;
+	public AudioClip crashSound;
+	private AudioSource playerAudio;
 
 	void Start()
 	{
 		playerRb = GetComponent<Rigidbody>();
 		playerAnim = GetComponent<Animator>();
 		Physics.gravity *= gravityModifier;
+		playerAudio = GetComponent<AudioSource>();
 	}
 
 	void Update()
@@ -25,6 +31,8 @@ public class PlayerController : MonoBehaviour
 			playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 			isOnGround = false;
 			playerAnim.SetTrigger("Jump_trig");
+			dirtParticle.Stop();
+			playerAudio.PlayOneShot(jumpSound, 1.0f);
 		}
 	}
 
@@ -34,6 +42,7 @@ public class PlayerController : MonoBehaviour
 		if (collision.gameObject.CompareTag("Ground"))
 		{
 			isOnGround = true;
+			dirtParticle.Play();
 		}
 		else if (collision.gameObject.CompareTag("Obstacle"))
 		{
@@ -41,6 +50,9 @@ public class PlayerController : MonoBehaviour
 			Debug.Log("Game Over!");
 			playerAnim.SetBool("Death_b", true);
 			playerAnim.SetInteger("DeathType_int", 1);
+			explosionParticle.Play();
+			dirtParticle.Stop();
+			playerAudio.PlayOneShot(crashSound, 1.0f);
 		}
 	}
 }
